ad765b250d8b02060a8f55fa0ca30b27ae36d0a8
Ashley Blair <ashleyblairchicago@gmail.com>
2021-06-28 16:18:18 -0500

    created app, modified read-me

--- src/App.js
+++ src/App.js
@@ -6,16 +6,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          ABLAIR DEV
+        
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Ashley Blair
         </a>
       </header>
     </div>
